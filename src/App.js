959cc9aa255b8f93284a07ef36e9f442675ed802
Reza Amraei <amraeireza@gmail.com>
2022-12-16 14:43:18 -0800

    feat: added react-router-dom to project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Main from "./Views/Main";
-import NavBar from "./Components/NavBar";
+import Build from "./Views/Build.js";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Main />} />
+          <Route exact path="/build" element={<Build />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
