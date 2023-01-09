a894b36a99b494b4d84ac75454bd761ca8f5e5d4
Reza Amraei <amraeireza@gmail.com>
2023-01-09 13:44:29 -0800

    feat: changed all image to file type .png

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "../CSS/SearchBar.css";
 import PokeAPIFunction from "../Utils/PokeAPIFunction";
 import DetermineHowManyTypes from "../Utils/DetermineHowManyTypes";
-const SearchBar = ({ setPokemonDisplay, setTypes, types }) => {
+const SearchBar = ({ setPokemonDisplay, setTypes, types, home = true }) => {
   const [pokemonForSearchBar, setPokemonForSearchBar] = useState("");
 
   const apiFunction = async function (pokemon) {
@@ -12,14 +12,17 @@ const SearchBar = ({ setPokemonDisplay, setTypes, types }) => {
     const typesFunctionResult = DetermineHowManyTypes(pokemonTest);
     setTypes([typesFunctionResult[0], typesFunctionResult[1]]);
   };
+  console.log(home);
   return (
     <div id="searchBar">
-      <div id="searchBarHeader">
-        <span className={types ? `${types[0]}` : ""}>
-          Search through &nbsp;{" "}
-        </span>
-        <span className={types ? `${types[1]}` : ""}> your PokeDex!</span>
-      </div>
+      {home && (
+        <div id="searchBarHeader">
+          <span className={types ? `${types[0]}` : ""}>
+            Search through &nbsp;{" "}
+          </span>
+          <span className={types ? `${types[1]}` : ""}> your PokeDex!</span>
+        </div>
+      )}
       <div className="searchBarInputFieldAndButton">
         <input
           type="text"
--- src/Views/Create.js
+++ src/Views/Create.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import ColumnOfPokeBalls from "../Components/ColumnOfPokeBalls";
 import NavBar from "../Components/NavBar";
 import SearchBar from "../Components/SearchBar";
 import "../CSS/Main.css";
 import "../CSS/Create-Team.css";
+import AddIcon from "@mui/icons-material/Add";
 
 const Create = () => {
+  const [buttonClicked, setButtonClicked] = useState(false);
   return (
     <div className="mainPage">
       <ColumnOfPokeBalls side="left" />
       <div className="middleColumn">
         <NavBar />
         <span className="createTeamFont createTeamHeader">Pick your team!</span>
+
+        <div className="createTeamMiddleCol">
+          {buttonClicked && <SearchBar home={false} />}
+          <button
+            className="createTeamButton"
+            onClick={() => setButtonClicked(true)}
+          >
+            <span>+</span> Add Pokemon
+          </button>
+        </div>
       </div>
       <ColumnOfPokeBalls side="right" />
     </div>
